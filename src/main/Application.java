e44fc31958cbe5f8bf35b7ff1173635b7ce2f6d8
Ivan Veseliak <ivan-veseliak@ukr.net>
2018-07-25 01:23:26 +0300

    Test the application.

--- src/main/Application.java
+++ src/main/Application.java
@@ -5,14 +5,20 @@ import institution.interlink.Internship;
 import person.Student;
 
 public class Application {
-    public static void main(String[] args) {
+    
+	public static void main(String[] args) {
         University university = new University("CH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko"));
-        university.addStudent(new Student("Julia Veselkina"));
-        university.addStudent(new Student("Maria Perechrest"));
+        university.addStudent(new Student("Andrew Kostenko", 20));
+        university.addStudent(new Student("Julia Veselkina", 30));
+        university.addStudent(new Student("Maria Perechrest", 40));
+        university.addStudent(new Student("Ivan Veseliak", 50));
+
 
         Internship internship = new Internship("Interlink");
+        internship.setStudents(university.getStudentsForInternship());
         System.out.println("List of internship's students:");
         System.out.println(internship.getStudents());
+
     }
+
 }
